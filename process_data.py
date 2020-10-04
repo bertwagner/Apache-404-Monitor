02dc473ba9154e0d458f98d768d214ba17fbeebe
Bert Wagner <bertwagner@bertwagner.com>
2020-10-03 20:31:12 -0400

    parse logs into dataframe

--- process_data.py
+++ process_data.py
@@ -1,6 +1,9 @@
 import smtplib
 import os
 import subprocess
+import re
+import json
+import pandas as pd
 
 FROM = os.environ["apache_404_monitor_email_from"]
 FROM_PASSWORD = os.environ["apache_404_monitor_password"]
@@ -9,16 +12,39 @@ TO = [os.environ["apache_404_monitor_email_to"]]
 BLOCKLIST = []
 
 
+class ApachLogEntry(object):
+    def __init__(self, raw_entry):
+        match = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?"([GET|POST|HEAD|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH].*?)" (\d{3}) (\d{1,10}) "(.*?)" "(.*?)"',raw_entry)
+        self.ip = match.group(1)
+        self.request = match.group(2)
+        self.status_code = match.group(3)
+        self.size = match.group(4)
+        self.referrer = match.group(5)
+        self.user_agent = match.group(6)
+        self.raw = raw_entry
+    
+    def to_dict(self):
+        return {
+            'ip':self.ip,
+            'request':self.request,
+            'status_code':self.status_code,
+            'size':self.size,
+            'referrer':self.referrer,
+            'user_agent':self.user_agent,
+            'raw':self.raw
+        }
+
 def FilterUninteresting404s():
 
-    list_of_404s = []
+    parsed_entries = []
 
     with open("access.log") as f:
         for line in f:
-            if "404" in line:
-                list_of_404s.append(line)
-    
-    print(list_of_404s)
+            parsed_entries.append(ApachLogEntry(line))
+            
+    df = pd.DataFrame.from_records([s.to_dict() for s in parsed_entries])
+    test=5
+
 
 def SendEmail():
     SUBJECT = 'test subject!'
@@ -35,9 +61,13 @@ def SendEmail():
     server.close()
     print('successfully sent the mail')
 
+def AddEntriesToBlockList():
+    # We don't want these same URLs to appear in future reports, so we add them to the block list
+    pass
 
 if __name__ == "__main__":
     FilterUninteresting404s()
-    SendEmail()
+    #SendEmail()
+    AddEntriesToBlockList()
 
     
