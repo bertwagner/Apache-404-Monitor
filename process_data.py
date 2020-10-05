59fc8a747aa0a269a07c540d3714eb77159b2434
Bert Wagner <bertwagner@bertwagner.com>
2020-10-05 06:12:08 -0400

    adds filtering of data and emails

--- process_data.py
+++ process_data.py
@@ -4,29 +4,29 @@ import subprocess
 import re
 import json
 import pandas as pd
+import numpy as np
 
 FROM = os.environ["apache_404_monitor_email_from"]
 FROM_PASSWORD = os.environ["apache_404_monitor_password"]
 TO = [os.environ["apache_404_monitor_email_to"]]
 
-BLOCKLIST = []
-
-
 class ApachLogEntry(object):
     def __init__(self, raw_entry):
-        match = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?"([GET|POST|HEAD|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH].*?)" (\d{3}) (\d{1,10}) "(.*?)" "(.*?)"',raw_entry)
+        match = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?"(GET|POST|HEAD|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH)(.*?)" (\d{3}) (\d{1,10}) "(.*?)" "(.*?)"',raw_entry)
         self.ip = match.group(1)
-        self.request = match.group(2)
-        self.status_code = match.group(3)
-        self.size = match.group(4)
-        self.referrer = match.group(5)
-        self.user_agent = match.group(6)
+        self.verb = match.group(2)
+        self.url = match.group(3)
+        self.status_code = match.group(4)
+        self.size = match.group(5)
+        self.referrer = match.group(6)
+        self.user_agent = match.group(7)
         self.raw = raw_entry
     
     def to_dict(self):
         return {
             'ip':self.ip,
-            'request':self.request,
+            'verb':self.verb,
+            'url':self.url,
             'status_code':self.status_code,
             'size':self.size,
             'referrer':self.referrer,
@@ -34,7 +34,14 @@ class ApachLogEntry(object):
             'raw':self.raw
         }
 
-def FilterUninteresting404s():
+def ReadInExclusionList():
+    try:
+        exclusion_list = pd.read_pickle("exclusion_list.pkl")
+        return exclusion_list
+    except Exception as e:
+        return pd.DataFrame()
+
+def FilterNew404s(exclusion_list):
 
     parsed_entries = []
 
@@ -42,13 +49,25 @@ def FilterUninteresting404s():
         for line in f:
             parsed_entries.append(ApachLogEntry(line))
             
-    df = pd.DataFrame.from_records([s.to_dict() for s in parsed_entries])
-    test=5
+    logs = pd.DataFrame.from_records([s.to_dict() for s in parsed_entries])
+    
+    logs_404s = logs[logs["status_code"] == "404"]
+
+    try:
+        new_logs_404s = logs_404s[~logs_404s.url.isin(exclusion_list.url)]
+    except:
+        new_logs_404s = logs_404s
+
+    return new_logs_404s
 
 
-def SendEmail():
-    SUBJECT = 'test subject!'
-    TEXT = 'this is the body of hte message!'
+def SendEmail(logs):
+    SUBJECT = 'Daily BertWagner.com 404s'
+    
+    TEXT = ''
+    
+    for url in logs["url"].unique():
+        TEXT += url + '\n'
 
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
@@ -59,15 +78,17 @@ def SendEmail():
     server.login(FROM, FROM_PASSWORD)
     server.sendmail(FROM, TO, message)
     server.close()
-    print('successfully sent the mail')
 
-def AddEntriesToBlockList():
+def WriteToExclusionList(exclusion_list,new_entries):
     # We don't want these same URLs to appear in future reports, so we add them to the block list
-    pass
+    new_exclusion_list = pd.concat([exclusion_list,new_entries])
+    new_exclusion_list.to_pickle("exclusion_list.pkl")
 
 if __name__ == "__main__":
-    FilterUninteresting404s()
-    #SendEmail()
-    AddEntriesToBlockList()
+    exclusion_list = ReadInExclusionList()
+    logs = FilterNew404s(exclusion_list)
+    if len(logs.index) > 0:
+        SendEmail(logs)
+        WriteToExclusionList(exclusion_list,logs)
 
     
